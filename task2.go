118bcf90dbeecb0ddb0c670896abba2d64681c22
biruk50 <89568667+biruk50@users.noreply.github.com>
2025-10-29 19:14:38 +0300

    Task 2 done

--- task2.go
+++ task2.go
@@ -2,17 +2,57 @@ package main
 
 import (
 	"fmt"
+	s "strings"
+	"unicode"
 )
-/* Fundamentals of Go Tasks
-Task:  Word Frequency Count
-Write a Go function that takes a string as input and returns a dictionary containing the frequency of each word in the string. Treat words in a case-insensitive manner and ignore punctuation marks.
-[Optional]: Write test for your function
 
-Task : Palindrome Check
-Write a Go function that takes a string as input and checks whether it is a palindrome or not. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
-[Optional]: Write test for your function
- */
+var print = fmt.Println
+
+func filter(input string) string {
+	var filtered []rune
+	for _,r:= range input {
+		if unicode.IsPunct(r) { continue }
+		r=unicode.ToLower(r)
+		filtered= append(filtered,r)
+	}
+	return string(filtered)
+}
+
+func word_frequency(target string) map[string]int {
+	words := s.Fields(target)
+	counter := make(map[string]int)
+	
+	
+	for _, word := range words {
+		word = filter(word)
+		counter[word]++
+	}
+
+	return counter
+}
+
+
+
+func palindromeChecker(str string) bool {
+	str = s.ToLower(str)
+
+
+	var filtered []rune
+	for _, r := range str {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			filtered = append(filtered, r)
+		}
+	}
+
+	n := len(filtered)
+	for i := 0; i < n/2; i++ {
+		if filtered[i] != filtered[n-1-i] { return false }
+	}
+
+	return true
+}
 
 func main() {
-	
+	print(word_frequency("the angry the "))
+	print(palindromeChecker("heleh"))
 }
